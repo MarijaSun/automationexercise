1f26750d49a4f304dbbe9263a1b288b9eb9611c7
DESKTOP-VAN0OKR\Marija <marija7bg@gmail.com>
2022-12-24 11:57:23 +0100

    Initial Commit

---
+++ AutomationExerciseFramework/Features/Authentication.feature
@@ -0,0 +1,20 @@
+﻿Feature: Authentication
+	As a user
+	I want to be able to authenticate to the app
+	So I can work with restricted web app content
+
+@mytag
+Scenario: User can log in
+	Given user opens sign in page
+	  And enters correct credentials
+	When user submits login form
+	Then user will be logged in
+
+Scenario: User can sign up
+	Given user opens sign in page
+	  And enters 'Marija' name and valid email adress
+	  And user clicks on SignUp button
+	When user fills in all required field
+	  And submits the signup form
+	Then user will get 'Account Created!' success message
+	  And user will be logged in
---
+++ AutomationExerciseFramework/Helpers/TestConstants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationExerciseFramework.Helpers
+{
+    public static class TestConstants
+    {
+        //existing user data
+        public const string Username = "marijasunjka@gmail.com";
+        public const string Password = "QAkurs011";
+
+        //new user data
+        public const string FirstName = "Marija";
+        public const string LastName = "Sunjka";
+        public const string Address = "Vojvode Stepe 344";
+        public const string Country = "Canada";
+        public const string State = "Ontario";
+        public const string City = "Montreal";
+        public const string ZipCode = "10010";
+        public const string Phone = "0000100100";
+    }     
+}
